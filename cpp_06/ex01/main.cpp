ef64364b8eefc79f727c77cccec7077ac3d42bce
Rahimov Farhod <farhod.rahimov0055@gmail.com>
2021-05-10 18:30:19 +0300

    ex01 done

--- cpp_06/ex01/main.cpp
+++ cpp_06/ex01/main.cpp
@@ -8,9 +8,9 @@ int main()
     raw = serialize();
     data = deserialize(raw);
 
-    std::cout << "random string 1: " << data->s1 << std::endl;
-    std::cout << "random int:      " << data->i << std::endl;
-    std::cout << "random string 2: " << data->s2 << std::endl;
+    std::cout << "random string 1 AFTER serialization: " << data->s1 << std::endl;
+    std::cout << "random int AFTER serialization:      " << data->i << std::endl;
+    std::cout << "random string 2 AFTER serialization: " << data->s2 << std::endl;
     delete data->s1;
     delete data->s2;
 }
--- cpp_06/ex01/serialize.cpp
+++ cpp_06/ex01/serialize.cpp
@@ -9,6 +9,10 @@ void * serialize(void)
     result->i = rand();
     result->s1 = getRandomString(1);
     result->s2 = getRandomString(6);
-
+    
+    std::cout << "random string 1 BEFORE serialization: " << result->s1 << std::endl;
+    std::cout << "random int BEFORE serialization:      " << result->i << std::endl;
+    std::cout << "random string 2 BEFORE serialization: " << result->s2 << std::endl;
+    std::cout << "------------------------------------" << std::endl;
     return (reinterpret_cast<void *>(result));
 }
